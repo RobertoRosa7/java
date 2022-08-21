1ec847a4644303a1406af1cc5ff36d8158e7f850
Roberto Rosa <roberto.rosa7@gmail.com>
2022-08-21 10:05:06 -0300

    spring REST API - autenticação com JWT

---
+++ forum/src/main/java/br/com/alura/forum/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AutenticaoService autenticaoService;
+
+    // configuração de autenticação - usuários
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    // configuração de autorização - endponts - rotas
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/tocicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    // configuração de recursos estaticos - html, css, images
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("1234"));
+    }
+}
